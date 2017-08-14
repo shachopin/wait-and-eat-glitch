7e0026e14b7233d5a8ebee4b9f3e752a675943fe
System <none>
2017-08-14 23:18:17 +0000

    🍖🗼 Updated with Glitch

--- public/angularapp/waitList/config.route.js
+++ public/angularapp/waitList/config.route.js
@@ -20,7 +20,7 @@
     return authService.firebaseAuthObject.$requireSignIn(); //buit-in function by firebase that if the user is logged in, returns a promise that ruturns the user object, the promise will got rejected, if user is not logged-in
     //return a promise, if the promise can be resolved successully, then go to waitList controller, and user set to the return value of the resolve value
     //the user can be injected into WaitList controller
-    //if not, the WaitListController won't even be loaded (#/waitList page can still show, it's just the waitList controller do nothing)
+    //if not, the WaitListController won't even be loaded (#/waitList page can still show, it's just the waitList controller do nothing. hence a blank page)
     //if the promise got rejected, will trigger the $routeChangeError event on $rootScope (we want to capture that event and force user go to login page)
     //we will do that in app.module.js
   }
